05c8392e1dd30c99eeca7fa7d3481475e279213b
vishal kumar <76445546+WebVishal@users.noreply.github.com>
2025-01-21 22:22:38 +0530

    Update config.ts

--- lib/config.ts
+++ lib/config.ts
@@ -1,5 +1,5 @@
 export const config = {
-    publishableKey: "pk_test_c2V0dGxpbmctZ29waGVyLTc0LmNsZXJrLmFjY291bnRzLmRldiQ",
-    DataBaseUrl: "postgresql://aapna-rider_owner:LFQ8C5VuJBwY@ep-floral-river-a1s584tw.ap-southeast-1.aws.neon.tech/aapna-rider?sslmode=require",
-    STRIPE_SECRET_KEY: "sk_test_51QIRBp04X5xwc04GvUpvjQK49MFQgZiTH5Vp5zMMr2Vbd5cCUyj09CrdJBSkwpwSGNDnZMYDZxtoVIyxH4fKsJt700aLedKKK3"
+    publishableKey: process.env.PUBLISHABLE_KEY,
+    DataBaseUrl: process.env.DataBaseUrl,
+    STRIPE_SECRET_KEY: process.env.STRIPE_KEY
 };
